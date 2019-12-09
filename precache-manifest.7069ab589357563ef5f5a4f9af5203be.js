5f4f43f677b7b2a346a26de81211ef951ec0a3b9
Riko Riswandha <rikorisw@gmail.com>
2019-12-09 17:15:13 +0700

    Updates

---
+++ precache-manifest.7069ab589357563ef5f5a4f9af5203be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960ac4137c3013a7cc432d9e8413c346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6037a7e6d7e39e24a5e2",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5339c0b197c4f7207c0d",
+    "url": "/static/css/main.3202bce1.chunk.css"
+  },
+  {
+    "revision": "6037a7e6d7e39e24a5e2",
+    "url": "/static/js/2.54ad65e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.54ad65e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5339c0b197c4f7207c0d",
+    "url": "/static/js/main.be27b059.chunk.js"
+  },
+  {
+    "revision": "01e5a42a0a779b392583",
+    "url": "/static/js/runtime-main.58c14e21.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
