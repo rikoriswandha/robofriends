eec195de0493dd265f11f70256e999cdf00d9cc7
Riko Riswandha <rikorisw@gmail.com>
2019-12-09 16:53:22 +0700

    Completed roboapps react app

---
+++ src/components/CardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {
+        robots.map(robot => {
+          return (
+            <Card 
+              key={robot.id} 
+              id={robot.id} 
+              name={robot.name} 
+              email={robot.email}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default CardList;
